27e300c2df4b6637ea80f992de1f34765602ad7d
Vacha2 <160797812+Vacha11@users.noreply.github.com>
2026-04-17 20:48:43 +1000

    feat(auth): implemented login screen for existing users with Firebase authentication

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,13 +1,160 @@
+import 'package:campushub/screens/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'home_screen.dart';
+import 'package:campushub/services/auth_service.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
   @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final AuthService _authService = AuthService();
+  bool _isLoading = false;
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Center(
-        child: Text("Login screen will go here"), // Placeholder text for the login screen
+      body: Stack(
+        children: [
+          Container( // Background container with white color
+            color:Colors.white
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container( // Decorative container at the bottom with a gradient background
+              height: 75,
+              width: double.infinity,
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  colors:[
+                    Color(0xFFD6001C), // bright red
+                    Color(0xFFA6192E), // red
+                    Color(0xFF76232F), // deep red
+                  ],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                ),
+              ),
+            ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 40),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children:[
+                const SizedBox(height:120),
+                const Text(
+                  'Email',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.black,
+                  ),
+                ),
+                const SizedBox(height:8),
+
+                TextField(
+                  controller: _emailController,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.grey[200],
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
+
+                const SizedBox(height: 30),
+
+                const Text(
+                  'Password',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.black,
+                  ),
+                ),
+                const SizedBox(height:8),
+                TextField(
+                  controller: _passwordController,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.grey[200],
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 40),
+
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed:() async {
+                      setState(() {
+                        _isLoading = true;
+                      });
+                      String email = _emailController.text.trim();
+                      String password = _passwordController.text.trim();
+
+                      var user = await _authService.signIn(email, password);
+                      setState(() {
+                        _isLoading = false;
+                      });
+                      if (user != null) {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => const HomeScreen()
+                        ),
+                      );
+                    } else {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text('Login failed. Please check your credentials and try again.')),
+                        );
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFFA6192E), // red
+                      padding: const EdgeInsets.symmetric(vertical: 14), // button padding
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10), 
+                      ),
+                    ),
+                    child: const Text(
+                      "Log In",
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.white,
+                      ),
+                    ),
+                      
+                  ),
+                ),
+                const SizedBox(height: 10),
+                TextButton(
+                  onPressed: () {
+                    // to do
+                  },
+                  child: const Text(
+                    "Forgot Password?",
+                    style: TextStyle(
+                      color: Color(0xFFA6192E), // red
+                      fontSize: 14,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
